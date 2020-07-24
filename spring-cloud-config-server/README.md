c123883410198458e00045d45c2cedbf0272d8e0
pdelebarre <pdelebarre@gmail.com>
2020-07-24 18:22:55 +0200

    almost final

---
+++ spring-cloud-config-server/README.md
@@ -0,0 +1,5 @@
+http://localhost:8888/limits-service/dev
+http://localhost:8888/limits-service/qa
+http://localhost:8888/limits-service/default
+
+### commit properties files !!
---
+++ spring-cloud-config-server/src/test/java/com/delebarre/idp/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delebarre.idp.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
